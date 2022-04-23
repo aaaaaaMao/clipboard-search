f7f9f0e271bd824c07351c3ffae69e2171d8f6d9
maojie <41260899+aaaaaaMao@users.noreply.github.com>
2022-04-23 22:06:41 +0800

    feat: Add window

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-from PyQt5.QtWidgets import QApplication
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import (
+    QApplication, 
+    QMainWindow,
+    QPlainTextEdit
+)
+from PyQt5.QtCore import QSize, Qt
 
-app = QApplication([])
-clipboard = app.clipboard()
+class MainWindow(QMainWindow):
 
-def read_clipboard():
-    data = clipboard.mimeData()
-	
-    if 'text/plain' in data.formats():
-        print(data.text())
+    def __init__(self):
+        QMainWindow.__init__(self)
 
-clipboard.dataChanged.connect(read_clipboard)
-app.exec_()
+        self.setMinimumSize(QSize(400, 240))
+        self.setWindowTitle("Clipboard search")
+
+        self.text_editor = QPlainTextEdit(self)
+        self.text_editor.insertPlainText('Waiting copy')
+        self.text_editor.move(10, 10)
+        self.text_editor.resize(380,220)
+
+        QApplication.clipboard().dataChanged.connect(self.read_clipboard)
+
+    def read_clipboard(self):
+        text = QApplication.clipboard().text()
+        
+        if self.text_editor.toPlainText() != text:
+            self.text_editor.setPlainText('')
+            self.text_editor.insertPlainText(text)
+
+            self.setWindowFlags(Qt.WindowStaysOnTopHint)
+            self.showNormal()
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit(app.exec_())
