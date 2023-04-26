eb866c509b1825710bfa38df1bf31055d08c5ccd
maojie <41260899+aaaaaaMao@users.noreply.github.com>
2023-04-26 23:29:54 +0800

    fix: filter none word

--- src/views/token_list.py
+++ src/views/token_list.py
@@ -17,7 +17,7 @@ class TokenList(QListWidget):
         tokens = []
         for item in tag(text):
             word_orth_base = item['orthBase']
-            if not word_orth_base in dup:
+            if word_orth_base and (not word_orth_base in dup):
                 dup.add(word_orth_base)
                 self.addItem(word_orth_base)
                 tokens.append(word_orth_base)
