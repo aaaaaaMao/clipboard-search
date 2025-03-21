529d6dbc5cec8ded5d6da3694b34e0cef5ef4895
Arashi <6spiralcat@gmail.com>
2025-03-21 23:01:11 +0800

    feat: update logs

--- src/__init__.py
+++ src/__init__.py
@@ -1,26 +1,34 @@
 import logging
+from logging.handlers import TimedRotatingFileHandler
 import os
 
 from src.config import ConfigManager
 
+cwd = os.getcwd()
+
+for dir in ['logs', 'data']:
+    full_dir = os.path.join(cwd, dir)
+    if not os.path.exists(full_dir):
+        os.makedirs(full_dir)
+
+handler = TimedRotatingFileHandler(
+    filename='logs/app.log',
+    when='midnight',
+    interval=1,
+    backupCount=7
+)
+
 logging.basicConfig(
     format='%(levelname)s - %(asctime)s - %(message)s',
-    filename='app.log',
-    filemode='a',
     encoding='utf8',
-    level=logging.INFO
+    level=logging.INFO,
+    handlers=[handler]
 )
 
 config_manager = ConfigManager()
 config = config_manager.config
 
 word_list_style_sheet = ''
-
-cwd = os.getcwd()
-data_dir = os.path.join(cwd, './data')
-
-if not os.path.exists(data_dir):
-    os.mkdir(data_dir)
 
 style_sheet_file = os.path.join(
     os.getcwd(), './data/dictionary/db/PJE4_2.css')
